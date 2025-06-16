f8bd2173c62d12f8a02f3dec8db08bdd5e10f675
eliseyishkaev <elisey654321@mail.ru>
2025-06-16 22:31:25 +0300

    start fast api

--- api/app/main.py
+++ api/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 
 # 1. Инициализация приложения
 app = FastAPI(
@@ -16,3 +17,11 @@ async def root():
 @app.get("/health")
 async def health_check():
     return {"status": "OK"}
+
+if __name__ == "__main__":
+     uvicorn.run(
+        "main:app",
+        host="0.0.0.0",  # Доступ с любого IP
+        port=8000,       # Порт 8000
+        reload=True      # Автоперезагрузка (опционально)
+    )
